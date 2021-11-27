c35b1fccddb666dec30e2af391e3daaf8f643c6f
Matěj Přerovský <bofin@skaut.cz>
2021-11-28 00:21:38 +0100

    First commit with velostat foil

--- PicoMidiDrums.ino
+++ PicoMidiDrums.ino
@@ -1,26 +1,31 @@
 #include <Arduino.h>
 #include <Adafruit_TinyUSB.h>
 #include <MIDI.h>
+#include <stdlib.h>
 
 #define PIN 16
 #define NUM 4
+#define STEP 12
 
 bool old[NUM] = {HIGH};
 Adafruit_USBD_MIDI usb_midi;
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDI);
 
 volatile int note = 60;
+int sensorPin = A2;
+float calibration;
+int sensorValue = 0;
+float f;
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  for(int i = 0; i<NUM; i++){
-    pinMode((PIN + i), INPUT_PULLUP);
-  }
+  
   pinMode(14, INPUT_PULLUP);
   pinMode(15, INPUT_PULLUP);
   attachInterrupt(14, upOctave, FALLING);
   attachInterrupt(15, downOctave, FALLING);
-  
+
+  calibration = analogRead(A2);
   // usb_midi.setStringDescriptor("TinyUSB MIDI");
   MIDI.begin(MIDI_CHANNEL_OMNI);
     
@@ -30,38 +35,28 @@ void setup()
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
+  
+  f = calibration/127;
 }
 
 void loop()
 {
-  for(int i = 0; i<NUM; i++)
-  {
-    bool nev = digitalRead(PIN + i);
-    if(old[i] != nev)
-    {
-      if(nev == LOW){
-        MIDI.sendNoteOn(note + i, 127, 1+i);
-        digitalWrite(LED_BUILTIN, HIGH);
-      }
-      else{
-        MIDI.sendNoteOff(note + i, 0, 1+i);
-        digitalWrite(LED_BUILTIN, LOW);
-      }
-      old[i] = nev;
-    }
+  sensorValue = analogRead(sensorPin);
+  if(sensorValue >= calibration)
+    MIDI.sendNoteOff(note, 0, 1);
+  else {
+    MIDI.sendNoteOn(note, 127, 1);
+    // abs(ceil(sensorValue/f)-127)
   }
+  
 }
 void upOctave(){
-  if(note<=(127-12)){
-    for(int i = 0; i<NUM; i++)
-      MIDI.sendNoteOff(note + i, 0, 1+i);
-    note += 12;
-  }
+  for(int i = 0; i<NUM; i++)
+   MIDI.sendNoteOff(note + i, 0, 1+i);
+  note += (note<=(127-STEP)) ? STEP : 0;
 }
 void downOctave(){
-  if(note>=(12)){
-    for(int i = 0; i<NUM; i++)
-      MIDI.sendNoteOff(note + i, 0, 1+i);
-    note -= 12;
-  }
+  for(int i = 0; i<NUM; i++)
+    MIDI.sendNoteOff(note + i, 0, 1+i);
+  note -= (note>=(STEP)) ? STEP : 0;
 }
