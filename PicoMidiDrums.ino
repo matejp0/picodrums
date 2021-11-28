3d7d2bd91276df586f05c8b6fa0eca36157cd0e8
Matěj Přerovský <bofin@skaut.cz>
2021-11-28 13:04:43 +0100

    Cleaned up and improved the code

--- PicoMidiDrums.ino
+++ PicoMidiDrums.ino
@@ -3,60 +3,70 @@
 #include <MIDI.h>
 #include <stdlib.h>
 
-#define PIN 16
-#define NUM 4
+#define SENSORPIN A2
 #define STEP 12
 
-bool old[NUM] = {HIGH};
 Adafruit_USBD_MIDI usb_midi;
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDI);
 
-volatile int note = 60;
-int sensorPin = A2;
+volatile int note = 60; // middle C
+
 float calibration;
+
 int sensorValue = 0;
-float f;
+
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
   
   pinMode(14, INPUT_PULLUP);
   pinMode(15, INPUT_PULLUP);
-  attachInterrupt(14, upOctave, FALLING);
-  attachInterrupt(15, downOctave, FALLING);
+  attachInterrupt(14, downOctave, FALLING);
+  attachInterrupt(15, upOctave, FALLING);
 
-  calibration = analogRead(A2);
-  // usb_midi.setStringDescriptor("TinyUSB MIDI");
+  calibration = analogRead(SENSORPIN) - 100;
+  
   MIDI.begin(MIDI_CHANNEL_OMNI);
     
   Serial.begin(115200);
-
-  while(!USBDevice.mounted()) delay(1);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
   
-  f = calibration/127;
+  while(!USBDevice.mounted()) delay(1);
+  
+  digitalWrite(LED_BUILTIN, LOW);
+      
 }
 
 void loop()
 {
-  sensorValue = analogRead(sensorPin);
-  if(sensorValue >= calibration)
+  sensorValue = analogRead(SENSORPIN);
+  
+  if(sensorValue >= calibration+50)
+  {
     MIDI.sendNoteOff(note, 0, 1);
-  else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  
+  else
+  {
     MIDI.sendNoteOn(note, 127, 1);
-    // abs(ceil(sensorValue/f)-127)
+    digitalWrite(LED_BUILTIN, HIGH);
   }
   
 }
 void upOctave(){
-  for(int i = 0; i<NUM; i++)
+  for(int i = 0; i<1; i++)
    MIDI.sendNoteOff(note + i, 0, 1+i);
   note += (note<=(127-STEP)) ? STEP : 0;
 }
 void downOctave(){
-  for(int i = 0; i<NUM; i++)
+  for(int i = 0; i<1; i++)
     MIDI.sendNoteOff(note + i, 0, 1+i);
   note -= (note>=(STEP)) ? STEP : 0;
 }
+
+/*  TODO:
+ *  float f;
+ *  f = calibration/127;
+ *  abs(ceil(sensorValue/f)-127)
+ */
